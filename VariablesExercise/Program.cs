58410536568cfb414fdb47471c66bce47741af45
Katey McKenney <mckenneykatey@gmail.com>
2024-04-26 11:20:21 -0500

    Added 3 lines with with variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -8,9 +8,9 @@
 
             int dogAge = 4;
 
-            char lastNameInitial = 'M';
+            char lastInitial = 'M';
 
-            bool male = true;
+            bool badDog = true;
 
             double dogHeight = 20.4;
 
@@ -18,8 +18,9 @@
 
 
 
-            
-
+            Console.WriteLine($"My dog is a rat terrier, & his name is {dogName} {lastInitial}.");
+            Console.WriteLine($"{dogName} is approximately {dogHeight} inches tall and weighs {dogWeight} pounds.");
+            Console.WriteLine($"It's {badDog}, {dogAge} year old {dogName} LIVES to be naughty!");
         }
     }
 }
